f0398699eada3b72df5dff058eabc2dd8dca718b
mamorenoa <miguelangel.morenoalvarez@telefonica.com>
2021-03-01 22:17:03 +0100

    Avoid wildcard imports

--- app/src/main/java/com/example/androiddevchallenge/screen/detail/DoglyDetailScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/screen/detail/DoglyDetailScreen.kt
@@ -1,9 +1,23 @@
 package com.example.androiddevchallenge.screen.detail
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite

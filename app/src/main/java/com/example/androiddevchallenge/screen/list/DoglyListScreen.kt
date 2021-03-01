8636e25af9e856faf2f7d7ebd4de20a141e738e9
mamorenoa <miguelangel.morenoalvarez@telefonica.com>
2021-03-01 23:21:04 +0100

    spotless applied

--- app/src/main/java/com/example/androiddevchallenge/screen/list/DoglyListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/screen/list/DoglyListScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.screen.list
 
 import androidx.compose.foundation.background
@@ -8,8 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
-import com.example.androiddevchallenge.navigation.buildDogDetailRoute
 import com.example.androiddevchallenge.data.getDogs
+import com.example.androiddevchallenge.navigation.buildDogDetailRoute
 import com.example.androiddevchallenge.theme.yellow
 
 @Composable
